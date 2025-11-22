568b171e7c8fd43e4fc69ecc6dbb7ab9727056f5
alaahusam-lab <alaaalaahusam@gmail.com>
2025-11-22 07:23:45 +0200

    alaahusam

---
+++ src/main/kotlin/CsvParsevs.kt
@@ -0,0 +1,15 @@
+import com.bytebloom.model.raw.TeamRaw
+import java.io.File
+
+fun parseTeamData(): List<TeamRaw>{
+    val result = mutableListOf<TeamRaw>()//Create Empty List
+    val lines = File("src/main/resources/teams.csv").readLines()//Read The File
+
+    for (i in 1 until lines.size){
+        val parts = lines[i].split(", ")
+        val team = TeamRaw(parts[0], parts[1], parts[2])
+        result.add(team)
+    }
+    return result
+
+}
