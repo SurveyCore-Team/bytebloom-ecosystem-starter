5dc7f976682cd9a57a7cf40907e5d108b05255a0
alaahusam-lab <alaaalaahusam@gmail.com>
2025-11-23 16:17:11 +0200

    Edit the parseTeamData

--- src/main/kotlin/CsvParsevs.kt
+++ src/main/kotlin/CsvParsevs.kt
@@ -1,5 +1,7 @@
 import java.io.File
 import com.bytebloom.model.raw.PerformanceRaw
+import com.bytebloom.model.raw.TeamRaw
+
 fun parsePerformanceData():List<PerformanceRaw>{
     val file = File ("src/main/resources/performance.csv")
     val lines = file .readLines().drop(1)
@@ -11,4 +13,17 @@ fun parsePerformanceData():List<PerformanceRaw>{
             score=space1[3])
     }
     return space
+}
+
+
+val csvReadFile = File ("src/main/resources/teams.csv").readLines()
+val teamsList = mutableListOf<TeamRaw>()
+fun parseTeamData(): List<TeamRaw>{
+    for (lineIndex in 1 until csvReadFile.size){ //Start From Second Line.
+        val columns = csvReadFile[lineIndex].split(",") //Divid The Line to Colunms.
+        val teamRecord = TeamRaw(columns[0], columns[1], columns[2])
+
+        teamsList.add(teamRecord) //Add team to List
+    }
+    return teamsList
 }
