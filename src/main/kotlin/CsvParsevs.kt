153a60012a0eb29f0e691feee524c612053c737d
Raghad Abbas <munirraghad1612@gmail.com>
2025-12-12 11:53:47 +0200

    Test DomainBuilder and Some modifications

--- src/main/kotlin/CsvParsevs.kt
+++ src/main/kotlin/CsvParsevs.kt
@@ -3,14 +3,12 @@ import com.bytebloom.model.raw.PerformanceRaw
 import com.bytebloom.model.raw.TeamRaw
 import com.bytebloom.model.raw.MenteeRaw
 
-
 // Read all CSV files once (outside the functions)
 val performanceFileLines = File("src/main/resources/performance.csv").readLines().drop(1)
 val teamFileLines = File("src/main/resources/teams.csv").readLines().drop(1)
 val menteeFileLines = File("src/main/resources/mentees.csv").readLines().drop(1)
 
 fun parsePerformanceData():List<PerformanceRaw>{
-
     return performanceFileLines.map { line ->
         val columns = line.split(",")
         PerformanceRaw(menteeId = columns[0],
@@ -19,27 +17,18 @@ fun parsePerformanceData():List<PerformanceRaw>{
             score= columns[3])
     }
 }
-
-
-
-
 fun parseTeamData(): List<TeamRaw>{
     val teamsList = mutableListOf<TeamRaw>()
-    for (lineIndex in 1 until teamFileLines.size){ //Start From Second Line.
-        val columns = teamFileLines[lineIndex].split(",") //Divid The Line to Colunms.
+    for (lineIndex in 0 until teamFileLines.size){
+        val columns = teamFileLines[lineIndex].split(",")
         val teamRecord = TeamRaw(columns[0],
                               columns[1],
                                columns[2])
-
-        teamsList.add(teamRecord) //Add team to List
+        teamsList.add(teamRecord)
     }
     return teamsList
 }
-
-
-
 fun parseMenteeData(): List<MenteeRaw> {
-
     return menteeFileLines.map { line ->
         val columns = line.split(",")
         MenteeRaw(
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 
 fun main() {
-
     val mentees = parseMenteeData()
     val team = parseTeamData()
     val performance = parsePerformanceData()
@@ -8,22 +7,19 @@ fun main() {
     println("Mentee count = ${mentees.size}")
     println("Team count = ${team.size}")
     println("Performance rows = ${performance.size}")
+    println("____________________________________")
 
-
-        val builder = DomainBuilder()
-
-        val teams = builder.buildDomain(
+    val builder = DomainBuilder()
+    val teams = builder.buildDomain(
             performance.toMutableList(),
             team.toMutableList(),
             mentees.toMutableList()
         )
     val menteeList = builder.getMenteeList()
     val firstTeam = teams.first()
-    println("____________________________________")
-    println("First Team ID: ${firstTeam.teamId}")
+    println("First Team : ${firstTeam.Id}")
 
-    val menteesInsideThisTeam = menteeList.filter { it.teamId == firstTeam.teamId }
-
+    val menteesInsideThisTeam = menteeList.filter { it.teamId == firstTeam.Id }
     println("Mentees names Within the team :")
     menteesInsideThisTeam.forEach {
         println(it.name)
