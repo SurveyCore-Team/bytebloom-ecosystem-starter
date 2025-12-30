8b4c1b242656c4f5c454188609c8b90a08513e02
ALBatool8 <tot28820041@gmail.com>
2025-12-30 17:04:12 +0200

    updated MenteeRaw model to include submissions and attendanceRecords fields.

--- src/main/kotlin/models/MenteeRaw.kt
+++ src/main/kotlin/models/MenteeRaw.kt
@@ -1,9 +1,12 @@
 package models
 
 import domain.Attendance
+import domain.PerformanceSubmission
 
 data class MenteeRaw(
     val id:String,
     val name :String,
-    val teamId :String
+    val teamId :String,
+    var submissions: List<PerformanceSubmissionRaw>,
+    val attendanceRecords: List<AttendanceRaw>
 )
