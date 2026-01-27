6020aa4d465f802f3356871896be6fda7588fa33
ALBatool8 <tot28820041@gmail.com>
2026-01-27 12:11:27 +0200

    implement use case for fetching and ranking team performance

---
+++ src/main/kotlin/domain/usecase/performance/GetTeamPerformanceRankingUseCase.kt
@@ -0,0 +1,21 @@
+package domain.usecase.performance
+import domain.model.Team
+import domain.repository.TeamRepository
+import domain.usecase.BaseUseCase
+
+class GetTeamPerformanceRankingUseCase(
+    private val teamRepository: TeamRepository
+): BaseUseCase<Unit, List<Pair<Team, Double>>> {
+    override fun invoke(input: Unit): List<Pair<Team, Double>> =
+        teamRepository.getAllTeams()
+            .asSequence()
+            .map { team ->
+                team to team.members
+                    .asSequence()
+                    .flatMap { it.submissions.asSequence() }
+                    .map { it.score }
+                    .average()
+            }
+            .sortedByDescending { (_, average) -> average }
+            .toList()
+}
